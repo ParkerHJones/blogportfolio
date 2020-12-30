0c427062eef8f4ed5a3e5651ec0e7285746e9729
Parker Jones <parker.harold.jones@gmail.com>
2020-12-30 14:21:46 -0700

    Added link to github

--- src/containers/ProgrammingPortfolio/index.js
+++ src/containers/ProgrammingPortfolio/index.js
@@ -7,11 +7,14 @@ import './style.css';
  */
 
 
+
  const programming_portfolio = (props) => {
      return(
          <div>Programming Portfolio
              <br></br>
              Here are some projects of mine
+             <a href = "https://github.com/ParkerHJones?tab=repositories"> My GitHub </a>
+            
          </div>
      )
  }
